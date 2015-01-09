3e234ebbbc7171b750fbacd8fc4bda62ec563427
Mark Waldron <markewaldron@gmail.com>
2015-01-09 18:30:41 +0000

    Seeded database with 25 identical instances of a product, set up basic CSS for products page

---
+++ config/unicorn.rb
@@ -0,0 +1,11 @@
+working_directory "/home/depot/"
+
+pid "/home/depot/pid/unicorn.pid"
+
+stderr_path "/home/depot/log/unicorn.log"
+stdout_path "/home/depot/log/unicorn.log"
+
+listen "/tmp/unicorn.depot.sock"
+
+worker_processess 2
+timeout 30
