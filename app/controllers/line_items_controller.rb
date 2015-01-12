5adac93e7fce18021afaccb4a975af6e6b1a1c3f
Mark Waldron <markewaldron@gmail.com>
2015-01-12 12:12:24 -0500

    Added remove button, jquery hide on cart when emptied

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
 	include CurrentCart
 	before_action :set_cart, only: [:create]
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+    before_action :set_line_item, only: [:show, :edit, :update, :destroy, :decrement]
 
   def index
     @line_items = LineItem.all
@@ -51,6 +51,15 @@ class LineItemsController < ApplicationController
       format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+  
+  def decrement
+  	@line_item.decrement
+  	respond_to do |format|
+  		if @line_item.save
+  			format.html { redirect_to store_url }
+  		end
+  	end
   end
 
   private
